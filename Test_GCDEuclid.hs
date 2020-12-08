b1145233a711ffe0d829066bb9578c6556ccff9c
Pieter van Wyk <superfluidbcsbec@gmail.com>
2020-12-08 20:34:00 +0900

    Update Test_GCDEuclid.hs

--- Test_GCDEuclid.hs
+++ Test_GCDEuclid.hs
@@ -8,20 +8,6 @@ Unit tests of extended Euclid algorithm
 import GCDEuclid
 import Test.QuickCheck
 
--- Main Functions --
---------------------
-{-
--- extended Euclid algorithm implimentation.
--- returns gcd as well as certificate x,y
--- defined by gcd a b = a*x + b*y
-extended_gcd :: Int -> Int -> (Int, Int, Int)
-extended_gcd a 0 = (a, 1, 0)
-extended_gcd a b = let (d, p, q) = extended_gcd b (mod a b)
-                   in (d, q, p - q * (div a b))
-
--- END
--}
-
 -- Unit Tests --
 ----------------
 
