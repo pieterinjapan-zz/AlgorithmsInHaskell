77a33438ed210a4dfc99717e82145350890572a5
Pieter van Wyk <superfluidbcsbec@gmail.com>
2020-12-08 20:06:15 +0900

    Add files via upload

---
+++ BinarySearch.hs
@@ -0,0 +1,26 @@
+{-
+Author  : Pieter van Wyk
+Created : 2020-12-08
+Updated : 2020-12-08
+
+Implementation of binary earch algorithm
+-}
+module BinarySearch where
+
+-- Main Functions --
+--------------------
+
+binarySearch' :: Ord a => Int -> Int -> [a] -> a -> Maybe Int
+binarySearch' _ _ [] _ = Nothing
+binarySearch' minEl maxEl ls x | maxEl < minEl  = Nothing
+                               | x == ls!!guess = Just guess
+                               | x <  ls!!guess = binarySearch' minEl maxEl' ls x
+                               | otherwise      = binarySearch' minEl' maxEl ls x
+                               where guess = round $ 0.5 * fromIntegral (minEl + maxEl)
+                                     minEl' = guess + 1
+                                     maxEl' = guess - 1
+
+binarySearch :: Ord a => [a] -> a -> Maybe Int
+binarySearch ls = binarySearch' 0 (length ls - 1) ls
+
+-- END
---
+++ Test_BinarySearch.hs
@@ -0,0 +1,31 @@
+{-
+Author  : Pieter van Wyk
+Created : 2020-12-08
+Updated : 2020-12-08
+
+Implementation of binary earch algorithm
+-}
+import Test.QuickCheck
+import MergeSort
+import BinarySearch
+
+-- Unit Tests --
+----------------
+
+-- element inserted at given index is found at that index
+prop_binary_00 :: Int -> [Int] -> Bool
+prop_binary_00 el ls = binarySearch (small_ls ++ el:large_ls) el == Just idx
+  where small_ls = mergeSort [x | x <- ls, x < el]
+        large_ls = mergeSort [x | x <- ls, x < el]
+        idx = length small_ls
+
+-- element not in list is never found
+prop_binary_01 :: Int -> [Int] -> Bool
+prop_binary_01 el ls = binarySearch (small_ls ++ large_ls) el == Nothing
+  where small_ls = mergeSort [x | x <- ls, x < el]
+        large_ls = mergeSort [x | x <- ls, x < el]
+        idx = length small_ls
+
+main = do
+  quickCheck (withMaxSuccess 10000 prop_binary_00)
+  quickCheck (withMaxSuccess 10000 prop_binary_01)
