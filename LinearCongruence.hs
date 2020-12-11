49f800d2aef7e2f339f789a173e0cfec7f8f3c53
pieterinjapan <pieter-vanwyk@kke.co.jp>
2020-12-11 12:55:25 +0900

    Modified algorithm to use extended Euler algorithm instead of Diophantine equation solver

--- LinearCongruence.hs
+++ LinearCongruence.hs
@@ -8,14 +8,16 @@ linear congruence equations of the form n = r_a (mod a),
                                         n = r_b (mod b).
 -}
 module LinearCongruence where
-import LinearDiophantine
+import GCDEuclid
 
 -- Main Functions --
 --------------------
 
+-- 1) use extended Euclids algorithm to find (x,y) s.t. a*x + b*y = gcd a b
+-- 2) then (r_a*b*y + r_b*a*x) / gcd a b solves the congruence equations
 solveCon :: Int -> Int -> Int -> Int -> Int
 solveCon a b r_a r_b | mod (r_b - r_a) (gcd a b) /= 0 = error "no solution"
-                     | otherwise = let (x,y) = solveDio a b (r_b - r_a)
-                                   in x*a + r_a
+                     | otherwise = let (d, x, y) = extended_gcd a b
+                                   in div (r_a*b*y + r_b*a*x) d
 
 -- END
