ca47e3bd20e4bc84246c1dd63987f8b0b0dac8e7
pieterinjapan <pieter-vanwyk@kke.co.jp>
2020-12-14 10:23:44 +0900

    Added general solver for linear congruence equations where all moduli are coprime

--- NumericalAlgorithms/LinearCongruence.hs
+++ NumericalAlgorithms/LinearCongruence.hs
@@ -4,8 +4,7 @@ Created : 2020-12-09
 Updated : 2020-12-11
 
 Implimentation of algorithm for solving set of
-linear congruence equations of the form n = r_a (mod a),
-n = r_b (mod b) using the Chinese remainder theorem
+linear congruence equations using the Chinese remainder theorem
 -}
 module LinearCongruence where
 import GCDEuclid
@@ -13,6 +12,7 @@ import GCDEuclid
 -- Main Functions --
 --------------------
 
+-- solution of set of linear congruence equations of the form n = r_a (mod a), n = r_b (mod b)
 -- 1) use extended Euclids algorithm to find (x,y) s.t. a*x + b*y = gcd a b
 -- 2) then (r_a*b*y + r_b*a*x) / gcd a b solves the congruence equations
 solveCon :: Int -> Int -> Int -> Int -> Int
@@ -20,5 +20,16 @@ solveCon a b r_a r_b | mod (r_b - r_a) (gcd a b) /= 0 = error "no solution"
                      | otherwise = let (d, x, y) = extended_gcd a b
                                        n = div (r_a*b*y + r_b*a*x) d
                                    in mod n (a * b)
+
+-- solution of general set of linear congruence equations,
+-- assuming all moduli are coprime
+solveGenCon :: [(Int,Int)] -> Int
+solveGenCon = snd . solveGenCon'
+  where solveGenCon' ((r_a,a):ps) | null ps = (a,r_a)
+                                  | otherwise = let (prod',n') = solveGenCon' ps
+                                                    (_,x,y) = extended_gcd prod' a
+                                                    prod = prod'*a
+                                                    n = mod (r_a*prod'*x + n'*a*y) prod
+                                                in (prod, n)
 
 -- END
